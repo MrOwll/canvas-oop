f335da211e149039d630e1616757eb68afa3a75f
Kristina Garkovich <panda.bell.fran@gmail.com>
2022-08-08 22:57:34 +0300

    add canvas with squares

--- index.js
+++ index.js
@@ -1,163 +1,89 @@
-let PIECES=[];
-let SELECTED_PIECE=null;
+window.onload = function () {
+	const canvas = document.querySelector("canvas");
+	const context = canvas.getContext("2d");
+	const boundings = canvas.getBoundingClientRect();
 
+	let randomColor =`#${Math.floor(Math.random()*16777215).toString(16)}`;
 
-
-var canvas = document.getElementById("canvas");
-var a_context = canvas.getContext("2d");
-
-
-const height = 150;
-const widthGlobal = 150;
-
-const addFiguresList = () => {
-	const figuresArray = ['150', '200', '250', '300', '350', '400', '450', '500', '550'];	
-	const figuresList = document.getElementById('figure-list');
-
-	return figuresArray.map((item) => {
-		const figure = document.createElement('div');
-
-		figure.className = `figure ${item}`;
-		figure.textContent = item;
-		figure.dataset.size = item;
-
-		return figuresList.appendChild(figure);
-	});	
-}
-
-addFiguresList();
-
-class DrawFigure {
-	constructor(x1, y1, xSize, ySize){
-		this.x1 = x1;
-		this.y1 = y1;
-		this.xSize = xSize;
-		this.ySize = ySize;
-	}
-
-	draw(context){
-		context.fillStyle = '#ffb6b9';
-		context.beginPath();
-		context.rect(this.x1, this.y1, this.xSize, this.ySize);
-		context.lineWidth = 0.5;
-		context.fill();
-		context.stroke();
-	}
-}
-
-
-const figures = [];
-
-const loadData = () => {
-	// a_context.clearRect(0, 0, canvas.width, canvas.height);
+	class Square {
+		constructor(x, y, dimentions, color){
+			this.x = x;
+			this.y = y;
+			this.dimentions = dimentions;
+			this.color = color;
+		}
 	
-	figures.forEach(figure => {
-		const newFigure = new DrawFigure(figure.x, figure.y, figure.xSize, figure.ySize);
-		newFigure.draw(a_context);
-	})
-};
+		draw(context) {
+			context.fillStyle = this.color;
+			context.beginPath();
+			context.rect(this.x, this.y, this.dimentions, this.dimentions);
+			context.lineWidth = 0.5;
+			context.fill();
+			context.stroke();
+		};
+	}
 
-const dragndrop = (event, selectedFigure) => {
-	console.log(event);
-	const width = event.path[0].dataset.size;
-	console.log(width);
-
-	if (event.target.classList.contains('figure') || selectedFigure) {
-		let x = event.clientX;
-		let y = event.clientY;
-		let xSize =  width / Math.sqrt(2);
-		let ySize = height / Math.sqrt(2);
-		let figureType = '';
-
-		if (selectedFigure) {
-			figureType = selectedFigure.type;
-
-			figures.splice(figures.indexOf(selectedFigure), 1);
-
-			xSize = x - selectedFigure.x - (width * 2);
-			ySize = y - selectedFigure.y;
-
-			loadData();
-		} else figureType = event.target.classList[0];
-
-		dragFigure = document.createElement('div');
-		dragFigure.classList.add(figureType);
-		dragFigure.classList.add('drag');
-		dragFigure.style.left = x - xSize + 'px';
-		dragFigure.style.top = y - ySize + 'px';
-		document.body.appendChild(dragFigure);
-			console.log(xSize);
-
-	const mouseMove = (event) => {
-		let x = event.clientX;
-		let y = event.clientY;
-		dragFigure.style.left = x - ySize + 'px';
-		dragFigure.style.top = y - ySize + 'px';
-	};
-
-	document.addEventListener('mousemove', mouseMove);
-
-	const mouseUp = (event) => {
-		dragFigure.remove();
-		document.removeEventListener('mousemove', mouseMove);
-
-		let canvasX = event.clientX - (width * 2);
-		let canvasY = event.clientY;
-
-		if (canvasX > 0 && canvasX < canvas.width && canvasY > 0 && canvasY < canvas.height) {
-			figures.forEach(figure => {
-				figure.selected = false
-			});
-
-			dragFigure.selected = true;
-
-			figures.push({type: 'figure', x: canvasX - xSize, y: canvasY - ySize, selected: true});
-			
-			const newFigure = new DrawFigure(canvasX - xSize,  canvasY - ySize, xSize, ySize);
-			newFigure.draw(a_context);
-
-			console.log(newFigure);
-
-			loadData();
-			figureType = null;
+	class SquareWithNewColor extends Square {
+		constructor(x, y, dimentions, color){
+			super(x, y, dimentions, color);
 		}
 
-		document.removeEventListener('mouseup', mouseUp);
+		changeColor() {
+			this.color = randomColor;
+		}
 	}
 
-	document.addEventListener('mouseup', mouseUp);
-}
-}
+	const squares = [];
+	let currentSquare = null;
 
-	const selectFigure = (event) => {
-        let x = event.offsetX;
-        let y = event.offsetY;
+	const firstSquare = new Square(250, 250, 100, '#ffb6b9');
+	const secondSquare = new SquareWithNewColor(100, 100, 100, '#5290c0');
+	const therdSquare = new Square(400, 400, 100, '#63feb1');
+	
+	squares.push(firstSquare, secondSquare, therdSquare);
 
-        let selectedFigure = figures.find(figure => {
-            if (figure.x + figure.x > x
-                && figure.x <= x
-                && figure.y + height > y
-                && figure.y <= y) {
-                return figure
-            }
-        });
+	const drawSquares = () => {
+		context.clearRect(0, 0, canvas.width, canvas.height);
 
-        if (selectedFigure) {
-            dragndrop(event, selectedFigure)
-        } else {
-            figures.forEach(figure => figure.selected = false)
-        }
-    };
+		for (let index = 0; index < squares.length; index++) {
+			squares[index].draw(context);
+		}
+	}
 
-	const figureToDelete = (event) => {
-        if (event.key === "Backspace" || event.key === 'Delete') {
-            let figureToDelete = figures.find(figure => figure.selected === true);
-            figures.splice(figures.indexOf(figureToDelete), 1);
-            loadData();
-        }
-    };
+	drawSquares();
 
+	const isCursorInSquare = (mouseX, mouseY) => {
+		for (let index = squares.length - 1; index >= 0; index--) {
+			if(mouseX > squares[index].x &&
+				mouseX < squares[index].x + squares[index].dimentions &&
+				mouseY > squares[index].y &&
+				mouseY < squares[index].y + squares[index].dimentions) {
+				currentSquare = squares[index];	
+				currentSquare instanceof SquareWithNewColor && currentSquare.changeColor()
+			}
+		}
+	}
 
-	canvas.addEventListener('mousedown', selectFigure);
-    document.addEventListener('mousedown', dragndrop);
-    document.addEventListener('keydown', figureToDelete);
+	canvas.addEventListener('mousedown', (event) => {
+		const mouseDownX = event.clientX - boundings.left;
+		const mouseDownY = event.clientY - boundings.top;
+
+		isCursorInSquare(mouseDownX, mouseDownY);
+	})
+
+	canvas.addEventListener('mousemove', (event) => {
+		const mouseMoveX = event.clientX - boundings.left;
+		const mouseMoveY = event.clientY - boundings.top;
+
+		if (currentSquare) {
+			currentSquare.x = mouseMoveX;
+			currentSquare.y = mouseMoveY;
+
+			drawSquares();
+		}
+	})
+
+	canvas.addEventListener('mouseup', () => {
+		currentSquare = null;
+	})
+};
